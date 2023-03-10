1c084f0e6ae94e1ffeb901f67d9f8fdab9c1551d
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-03-10 08:20:40 -0300

    Added the column insurance_fee to store the calculated insurance fee

--- sql/create_vehicle_db.sql
+++ sql/create_vehicle_db.sql
@@ -7,6 +7,7 @@ create table car_info (
     brand varchar(30) not null,
     year_manufacture varchar(4) not null,
     price decimal(9,2) not null
+    insurance_fee decimal(6,2)
 );
 
 desc car_info
