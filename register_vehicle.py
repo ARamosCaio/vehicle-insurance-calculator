f3ccf3ff934bf8409969d04bcbf8152788dc1f64
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-03-10 08:19:00 -0300

    Created the function register_car to execute the vehicle registration

--- register_vehicle.py
+++ register_vehicle.py
@@ -1,29 +1,30 @@
 import mysql.connector
 
-connection = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "senha123",
-    database = "vehicles"
-)
+def register_car():
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "senha123",
+        database = "vehicles"
+    )
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
 
-sql = "INSERT INTO car_info (model, brand, year_manufacture, price) VALUES (%s, %s, %s, %s);"
+    sql = "INSERT INTO car_info (model, brand, year_manufacture, price) VALUES (%s, %s, %s, %s);"
 
-data = (
-    input('Insira o modelo do veículo: '),
-    input('Insira a marca do veículo: '),
-    input('Insira o ano de fabricação: '),
-    input('Insira o preço estimado do veículo: ')
-)
+    data = (
+        input('Insira o modelo do veículo: ')
+        input('Insira a marca do veículo: '),
+        input('Insira o ano de fabricação: '),
+        input('Insira o preço estimado do veículo: ')
+    )
 
-cursor.execute(sql, data)
-connection.commit()
+    cursor.execute(sql, data)
+    connection.commit()
 
-id = cursor.lastrowid
+    record_id = cursor.lastrowid
 
-cursor.close()
-connection.close()
+    cursor.close()
+    connection.close()
 
-print("Véiculo cadastrado. ID: ", id)
+    return("Veículo cadastrado. ID: ", record_id)
