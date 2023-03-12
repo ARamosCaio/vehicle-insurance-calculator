cd563979deb2727bfc70820cb44df533059ee58c
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-03-11 22:44:59 -0300

    Added the validation script when inserting the vehicle brand and year of manufacture

--- register_vehicle.py
+++ register_vehicle.py
@@ -1,30 +1,31 @@
 import mysql.connector
+import validate
 
-def register_car():
-    connection = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "senha123",
-        database = "vehicles"
-    )
+#def register_car():
+connection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "senha123",
+    database = "vehicles"
+)
 
-    cursor = connection.cursor()
+cursor = connection.cursor()
 
-    sql = "INSERT INTO car_info (model, brand, year_manufacture, price) VALUES (%s, %s, %s, %s);"
+sql = "INSERT INTO car_info (model, brand, year_manufacture, price) VALUES (%s, %s, %s, %s);"
 
-    data = (
-        input('Insira o modelo do veículo: '),
-        input('Insira a marca do veículo: '),
-        input('Insira o ano de fabricação: '),
-        input('Insira o preço estimado do veículo: ')
-    )
+data = (
+    input('Insira o modelo do veículo: '),
+    validate.validate_brand(),
+    validate.validate_year(),
+    input('Insira o preço estimado do veículo: ')
+)
 
-    cursor.execute(sql, data)
-    connection.commit()
+cursor.execute(sql, data)
+connection.commit()
 
-    record_id = cursor.lastrowid
+record_id = cursor.lastrowid
 
-    cursor.close()
-    connection.close()
+cursor.close()
+connection.close()
 
-    return("Veículo cadastrado. ID: ", record_id)
+("Veículo cadastrado. ID: ", record_id)
